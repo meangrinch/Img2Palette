2a2d059f32075c35dac2cf3ae3e994329b941218
Daniel Attwell <srirachaexpress@gmail.com>
2024-11-15 21:15:59 -0500

    Improved error handling

--- img2palette.py
+++ img2palette.py
@@ -1,6 +1,5 @@
-from tkinter import *
-from tkinter import filedialog
 import tkinter as tk
+from tkinter import filedialog, Scale, Frame, Label, Button, HORIZONTAL
 from PIL import Image, ImageTk
 from skimage import color
 from skimage.color import deltaE_ciede2000
@@ -13,91 +12,82 @@ class Img2Palette:
         self.img = None
         self.palette = None
         self.file_path = None
-
         self.setup_ui()
 
     def setup_ui(self):
         self.root.title("Img2Palette")
         self.root.geometry("250x420")
         self.root.resizable(False, False)
-
         self.image_frame = Frame(self.root, height=250)
         self.image_frame.pack()
-
         self.img_label = Label(self.root)
         self.img_label.pack()
-
-        get_image_button = Button(self.root, text="Select Image", command=self.get_image)
-        get_image_button.pack(pady=5)
-
-        scale_label = Label(self.root, text="Colors in Palette:")
-        scale_label.pack(pady=(5, 0))
-
+        Button(self.root, text="Select Image", command=self.get_image).pack(pady=5)
+        Label(self.root, text="Colors in Palette:").pack(pady=(5, 0))
         self.scale = Scale(self.root, from_=1, to=256, orient=HORIZONTAL, highlightthickness=0)
         self.scale.pack()
         self.scale.focus_set()
-
-        self.scale.bind("<Left>", self.decrease_scale)
-        self.scale.bind("<Down>", self.decrease_scale)
-        self.scale.bind("<Right>", self.increase_scale)
-        self.scale.bind("<Up>", self.increase_scale)
-
-        create_palette_button = Button(self.root, text="Save Color Palette", command=self.create_and_save_palette)
-        create_palette_button.pack(pady=5)
+        self.scale.bind("<Left>", self.change_scale(-1))
+        self.scale.bind("<Down>", self.change_scale(-1))
+        self.scale.bind("<Right>", self.change_scale(1))
+        self.scale.bind("<Up>", self.change_scale(1))
+        Button(self.root, text="Save Color Palette", command=self.create_and_save_palette).pack(pady=5)
 
     def get_image(self):
         self.file_path = filedialog.askopenfilename()
-        self.img = Image.open(self.file_path)
-        self.img = self.img.convert('RGB')
-        max_colors = min(len(set(self.img.getdata())), 256)
+        if not self.file_path:
+            return
+        self.img = Image.open(self.file_path).convert('RGB')
+        colors = np.array(self.img).reshape(-1, 3)
+        max_colors = min(len(np.unique(colors, axis=0)), 256)
         self.scale.config(to=max_colors)
-        max_size = (250, 250)
-        self.img.thumbnail(max_size, Image.LANCZOS)
+        self.img.thumbnail((250, 250), Image.Resampling.LANCZOS)
         self.image_frame.pack_forget()
-        img_preview = ImageTk.PhotoImage(self.img)
-        self.img_label.config(image=img_preview)
-        self.img_label.image = img_preview
+        self.img_preview = ImageTk.PhotoImage(self.img)
+        self.img_label.config(image=self.img_preview)  # type: ignore
 
     def create_and_save_palette(self):
-        num_colors = self.scale.get()
-        small_img = self.img.resize((256, 256))
-        result = small_img.quantize(colors=num_colors)
-        result = result.convert('RGB')
-        colors = result.getcolors(num_colors)
+        if self.img is None:
+            print("Please select an image first.")
+            return
+        num_colors = int(self.scale.get())
+        quantized_img = self.img.resize((256, 256)).quantize(colors=num_colors).convert('RGB')
+        lab_colors = self._sort_colors_by_lab(quantized_img.getcolors(num_colors))
+        self._save_palette(lab_colors, num_colors)
+
+    def _sort_colors_by_lab(self, colors):
         lab_colors = [color.rgb2lab(np.array([[col]])/255)[0][0] for count, col in colors]
         lab_colors.sort(key=lambda x: x[0])
         sorted_lab_colors = [lab_colors.pop(0)]
         while lab_colors:
             last_color = sorted_lab_colors[-1]
             distances = [deltaE_ciede2000(last_color, lab_color) for lab_color in lab_colors]
-            next_index = np.argmin(distances)
-            next_color = lab_colors.pop(next_index)
+            next_color = lab_colors.pop(np.argmin(distances))
             sorted_lab_colors.append(next_color)
-        sorted_colors = [color.lab2rgb(np.array([[lab_col]]))[0][0] for lab_col in sorted_lab_colors]
+        return [color.lab2rgb(np.array([[lab_col]]))[0][0] for lab_col in sorted_lab_colors]
+
+    def _save_palette(self, sorted_colors, num_colors):
+        if self.file_path is None:
+            print("Please select an image first.")
+            return
         swatchsize = 3
-        self.palette = Image.new('RGB', (swatchsize*num_colors, swatchsize))
-        posx = 0
-        for col in sorted_colors:
-            col = tuple(map(lambda x: int(round(x * 255)), col))
-            self.palette.paste(col, (posx, 0, posx+swatchsize, swatchsize))
-            posx += swatchsize
-        file_name = os.path.basename(self.file_path)
-        file_name = os.path.splitext(file_name)[0] + "_color_palette.png"
+        self.palette = Image.new('RGB', (swatchsize * num_colors, swatchsize))
+        for i, col in enumerate(sorted_colors):
+            col_int = tuple(map(lambda x: int(round(x * 255)), col))
+            self.palette.paste(col_int, (i * swatchsize, 0, (i + 1) * swatchsize, swatchsize))
+        file_name = os.path.splitext(os.path.basename(self.file_path))[0] + "_color_palette.png"
         save_file_path = filedialog.asksaveasfilename(defaultextension=".png", initialfile=file_name)
         if save_file_path:
             self.palette.save(save_file_path)
 
-    def decrease_scale(self, event):
-        current_value = self.scale.get()
-        new_value = max(current_value - 1, self.scale.cget("from"))
-        self.scale.set(new_value)
-        return "break"
-
-    def increase_scale(self, event):
-        current_value = self.scale.get()
-        new_value = min(current_value + 1, self.scale.cget("to"))
-        self.scale.set(new_value)
-        return "break"
+    def change_scale(self, delta):
+        def handler(event):
+            current_value = self.scale.get()
+            new_value = current_value + delta
+            if self.scale.cget("from") <= new_value <= self.scale.cget("to"):
+                self.scale.set(new_value)
+            return "break"
+        return handler
 
 def main():
     root = tk.Tk()
