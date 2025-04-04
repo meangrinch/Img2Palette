6f7140ce7603f575048baea2878ce0e954324bc5
grinnch <srirachaexpress@gmail.com>
2025-04-04 19:02:39 -0400

    Add metadata and update version

--- img2palette/__init__.py
+++ img2palette/__init__.py
@@ -1,3 +1,13 @@
-from .img2palette import Img2Palette
+"""
+Img2Palette: A simple tool to extract color palettes from images.
+"""
 
-__version__ = "1.0.1"
+from .img2palette import Img2Palette, main
+
+__version__ = "1.1.1"
+__version_info__ = (1, 1, 1)
+__author__ = "grinnch"
+__license__ = "MIT"
+__copyright__ = "Copyright 2023-present grinnch"
+__description__ = "A tool for extracting color palettes from images"
+__all__ = ['Img2Palette', 'main']
